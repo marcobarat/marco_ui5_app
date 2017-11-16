2235eeb86648057b24c8de13742bd37958d34f0a
MarcoPC-TOSH\Marco PC <marco.baratella@techedgegroup.com>
2017-11-16 18:34:37 +0100

    aggiunto start operation e refresh automtico della lista delle operazioni

--- app/controller/MainPod.controller.js
+++ app/controller/MainPod.controller.js
@@ -7,7 +7,6 @@ sap.ui.define([
     'sap/ui/core/mvc/Controller'
 ], function (jQuery, JSONModel, MessageToast, MessageBox, History, Controller) {
     "use strict";
-
     var MainPodController = Controller.extend("myapp.controller.MainPod", {
         workcenterid: null,
         shoporderid: null,
@@ -16,13 +15,11 @@ sap.ui.define([
         stepid: null,
         shoporder: null,
         sfc: null,
-        test:null,
-        
+        test: null,
         onInit: function () {
             this.router = sap.ui.core.UIComponent.getRouterFor(this);
             this.router.attachRoutePatternMatched(this.handleRouteMatched, this);
             this.initPod();
-
         },
         initPod: function () {
             this.user = sap.ui.getCore().getModel().getData().informations.user;
@@ -32,8 +29,6 @@ sap.ui.define([
             this.stepid = sap.ui.getCore().getModel().getData().informations.stepid;
             this.shoporderid = sap.ui.getCore().getModel().getData().informations.shoporderid;
             this.sfc = sap.ui.getCore().getModel().getData().informations.sfc;
-            this.shoporderid = sap.ui.getCore().getModel().getData().informations.shoporder;
-            
             this.test = new JSONModel();
             this.test.setData(sap.ui.getCore().getModel().getData().informations);
             this.getView().setModel(this.test);
@@ -59,17 +54,48 @@ sap.ui.define([
         update: function () {
             if (typeof sap.ui.getCore().getModel().getData().informations != null) {
                 this.initPod();
-
             } else {
                 alert("error");
             }
         },
-        startOperations: function (oEv) {
-            alert("oev");
+        startOperations: function (event) {
+            var opselected = sap.ui.getCore().getModel().getData().operationselected;
+            if (typeof opselected === "undefined") {
+                MessageToast.show("Select an operation first!");
+            } else {
+                if (opselected.status === "In queue") {
+                    this.performStart(opselected);
+                } else {
+                    MessageToast.show("Select an operation in queue!");
+                }
+            }
+        },
+        performStart: function (rowSelected) {
+            var transactionName = "StartOperation";
+            var that = this;
+            var site = "iGuzzini";
+            var input = "&plant=" + this.plantid + "&operationid=" + rowSelected.operation_id + "&shoporderid=" + this.shoporderid + "&sfc=" + this.sfc + "&user=" + this.user + "&workcenterid=" + this.workcenterid + "&resourceid=";
+            var transactionCall = site + "/Transaction" + "/" + transactionName;
+            jQuery.ajax({
+                url: "/XMII/Runner?Transaction=" + transactionCall + input + "&OutputParameter=JSON&Content-Type=text/xml",
+                method: "GET",
+                async: false,
+                success: function (oData) {
+                    var result = JSON.parse(oData.documentElement.textContent);
+                    if (result.error == "0" || result.error == 0) {
+                        var oEventBus = sap.ui.getCore().getEventBus();
+                        oEventBus.publish("MainPod", "updateOperation");
+
+                    } else {
+                        MessageToast.show("Errore! " + result.errorMessage);
+                    }
+                },
+                error: function (oData) {
+                    alert("errore");
+                }
+            });
         }
 
     });
-
     return MainPodController;
-
 });
--- app/controller/Operations.controller.js
+++ app/controller/Operations.controller.js
@@ -17,7 +17,11 @@ sap.ui.define([
         onInit: function () {
             this.router = sap.ui.core.UIComponent.getRouterFor(this);
             this.router.attachRoutePatternMatched(this.handleRouteMatched, this);
-            this.getView().setModel(this.initOperations());
+
+            var oEventBus = sap.ui.getCore().getEventBus();
+            oEventBus.subscribe("MainPod", "updateOperation", this.initOperationsB, this);
+
+            this.initOperations();
         },
         onAfterRendering: function () {
 
@@ -25,6 +29,9 @@ sap.ui.define([
 
         },
         initOperations: function () {
+
+
+
             this.user = sap.ui.getCore().getModel().getData().informations.user;
             this.workcenterid = sap.ui.getCore().getModel().getData().informations.workcenterid;
             this.plantid = sap.ui.getCore().getModel().getData().informations.plant;
@@ -36,21 +43,117 @@ sap.ui.define([
             var site = "iGuzzini";
             var input = "&plant=" + this.plantid + "&shoporderid=" + this.shoporderid + "&stepid=" + this.stepid;
             var transactionCall = site + "/XACQuery" + "/" + transactionName;
+            sap.ui.core.BusyIndicator.show();
 
             jQuery.ajax({
                 url: "/XMII/Illuminator?QueryTemplate=" + transactionCall + input + "&Content-Type=text/json",
                 method: "GET",
-                async: false,
+                async: true,
                 success: function (oData) {
                     oModel.setData(oData.Rowsets.Rowset[0].Row);
+                    that.getView().setModel(oModel);
+                    sap.ui.core.BusyIndicator.hide();
+
                 },
                 error: function (oData) {
                     that.error(oData);
                 }
             });
-            sap.ui.getCore().getModel().setProperty("/operations", 
-                oModel.getData());
+            sap.ui.getCore().getModel().setProperty("/operations",
+                    oModel.getData());
             return oModel;
+        },
+        initOperationsB: function () {
+            var oView = this.getView(); 
+            this.user = sap.ui.getCore().getModel().getData().informations.user;
+            this.workcenterid = sap.ui.getCore().getModel().getData().informations.workcenterid;
+            this.plantid = sap.ui.getCore().getModel().getData().informations.plant;
+            this.stepid = sap.ui.getCore().getModel().getData().informations.stepid;
+            this.shoporderid = sap.ui.getCore().getModel().getData().informations.shoporderid;
+            var oModel = new JSONModel();
+            var transactionName = "XAC_GetAllPhaseOperation";
+            var that = this;
+            var site = "iGuzzini";
+            var input = "&plant=" + this.plantid + "&shoporderid=" + this.shoporderid + "&stepid=" + this.stepid;
+            var transactionCall = site + "/XACQuery" + "/" + transactionName;
+            sap.ui.core.BusyIndicator.show();
+
+            jQuery.ajax({
+                url: "/XMII/Illuminator?QueryTemplate=" + transactionCall + input + "&Content-Type=text/json",
+                method: "GET",
+                async: true,
+                success: function (oData) {
+                    oModel.setData(oData.Rowsets.Rowset[0].Row);
+                    oView.setModel(oModel);
+                    sap.ui.core.BusyIndicator.hide();
+
+                },
+                error: function (oData) {
+                    //that.error(oData);
+                }
+            });
+            sap.ui.getCore().getModel().setProperty("/operations",
+                    oModel.getData());
+            return oModel;
+        },
+        rowOpSelected: function (event) {
+
+            var obj = event.getSource();
+            var empty = {d: false, w: false};
+
+            var tmpSelectedOp = obj.getBindingContext().getObject();
+            if (tmpSelectedOp === undefined) {
+                tmpSelectedOp.isSelected = false;
+            }
+            tmpSelectedOp.isSelected = tmpSelectedOp.isSelected === true ? false : true;
+            if (true === tmpSelectedOp.isSelected) {
+                obj.addStyleClass("selectedRow");
+                sap.ui.getCore().getModel().setProperty("/operationselected",
+                        tmpSelectedOp);
+                this.selectedOp = tmpSelectedOp;
+            } else {
+                obj.removeStyleClass("selectedRow");
+                this.selectedOp = empty;
+                sap.ui.getCore().getModel().setProperty("/operationselected",
+                        null);
+            }
+            var operations = this.getView().getModel().getData();
+
+            //var operations = this.operationsModel.getProperty("/operations");
+            for (var oi = 0, coi; oi < operations.length; oi++) {
+                coi = operations[oi];
+                if (coi.id !== tmpSelectedOp.id) {
+                    coi.isSelected = false;
+                }
+            }
+
+            var tab1 = this.getView().byId("tab1");
+
+            var items = tab1.getItems();
+
+            for (var i = 0, cobj; i < items.length; i++) {
+                cobj = items[i];
+                if (cobj.getId() !== obj.getId()) {
+                    cobj.removeStyleClass("selectedRow");
+                }
+            }
+
+            this.getView().getModel().refresh(true);
+
+        },
+        refreshSelection: function () {
+
+            var tab1 = this.getView().byId("tab1");
+            var items = tab1.getItems();
+
+            for (var i = 0, cobj, modelObj; i < items.length; i++) {
+                cobj = items[i];
+                modelObj = cobj.getBindingContext("ops").getObject();
+                if (modelObj.isSelected === true) {
+                    cobj.addStyleClass("selectedRow");
+                    break;
+                }
+            }
         },
         handleRouteMatched: function (oEvent) {
             if (!this._checkRoute(oEvent, "mainpod")) {
